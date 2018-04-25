33e1adf7ddef1671af30f12916286c5d61e6b583
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-04-25 21:59:00 +0700

    Create Product model

---
+++ src/models/product.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const config = require("../config/app");
+
+mongoose.connect(config.database);
+
+const Product = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    stock: {
+      type: Number,
+      required: true
+    },
+    totalViews: {
+      type: Number,
+      default: 0
+    },
+    photo: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Product", Product);
