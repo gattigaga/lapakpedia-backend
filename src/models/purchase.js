51ee53cbfaa0649727857987506cc6ce637cb3e6
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-04-26 20:26:44 +0700

    Create Purchase model

---
+++ src/models/purchase.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const config = require("../config/app");
+
+mongoose.connect(config.database);
+
+const Purchase = new mongoose.Schema(
+  {
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    },
+    totalPrices: {
+      type: Number,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Purchase", Purchase);
