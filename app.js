531e0958ae02a189bd242b1e088e90f6bad286c5
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-05-09 21:22:42 +0700

    Permit public folder to be access

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const compression = require("compression");
 const { ExtractJwt, Strategy } = require("passport-jwt");
+const path = require("path");
 
 const config = require("./src/config/app");
 const routes = require("./src/config/routes");
@@ -39,6 +40,7 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 routes(app);
 
